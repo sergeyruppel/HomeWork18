40c281ae852d71c9b2df873abbc64e3dd15b0798
Sergey Ruppel <sergeyruppel@gmail.com>
2023-09-23 19:10:11 +0700

    Part 1

--- ContactList/ContactList/Controllers/SimpleTableViewController.swift
+++ ContactList/ContactList/Controllers/SimpleTableViewController.swift
@@ -19,15 +19,15 @@ class SimpleTableViewController: UITableViewController {
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         self.navigationItem.rightBarButtonItem = self.editButtonItem
-        self.tableView.isEditing = true
+//        self.tableView.isEditing = true
     }
 
     // MARK: - Table view data source
 
 //    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 10
+//        return 1
 //    }
+    
     // Number of rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Person.getPersons().count
@@ -44,68 +44,60 @@ class SimpleTableViewController: UITableViewController {
     }
 
     // Reorder cells
-    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let movedObject = self.persons[sourceIndexPath.row]
-        persons.remove(at: sourceIndexPath.row)
-        persons.insert(movedObject, at: destinationIndexPath.row)
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
-        if editingStyle == .delete {
-            persons.remove(at: indexPath.row)
-            tableView.beginUpdates()
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            tableView.endUpdates()
-        }
-    }
-    
-//    
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection
-//                            section: Int) -> String? {
-//        return "Header \(section)"
+//    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+//        let movedObject = self.persons[sourceIndexPath.row]
+//        persons.remove(at: sourceIndexPath.row)
+//        persons.insert(movedObject, at: destinationIndexPath.row)
 //    }
-//    
+//
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+//        if editingStyle == .delete {
+//            tableView.beginUpdates()
+//            persons.remove(at: indexPath.item)
+//            tableView.deleteRows(at: [indexPath], with: .automatic)
+//            tableView.endUpdates()
+//        }
+//    }
+//
 
 
 
-    /*
+ 
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            persons.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+//        
+//        
+//        else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }    
     }
-    */
 
-    /*
+
+
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
 
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+
 
     /*
     // MARK: - Navigation
@@ -117,4 +109,9 @@ class SimpleTableViewController: UITableViewController {
     }
     */
 
+    
+    
+    deinit {
+        print("DECONSTRUCT")
+    }
 }
